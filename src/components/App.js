95dbd66813375b61e03d0ea78c6612a3a4118088
accio-rajitha <rajithasabhavath2019@gmail.com>
2025-01-06 17:36:38 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 // src/App.js
-// src/App.js
 import React from 'react';
 import { AuthProvider } from './AuthContext';
 import Auth from './Auth';
@@ -16,25 +15,3 @@ const App = () => {
 };
 
 export default App;
-/*
-import React from 'react';
-//import { AuthContext } from './AuthContext';
-
-import { AuthProvider } from './AuthContext';
-import Auth from './Auth';
-import './../styles/App.css';
-
-const App = () => {
-  return (
-    <AuthProvider>
-      <div className="App">
-        <h1>Click on the checkbox to get authenticated</h1>
-        <Auth />
-      </div>
-    </AuthProvider>
-  );
-};
-
-export default App;
-
-*/
--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -9,31 +9,9 @@ export const AuthProvider = ({ children }) => {
   const toggleAuth = () => {
     setIsAuthenticated((prev) => !prev);
   };
-
   return (
     <AuthContext.Provider value={{ isAuthenticated, toggleAuth }}>
       {children}
     </AuthContext.Provider>
   );
 };
-/*
-import React, { createContext, useState } from 'react';
-
-const AuthContext = createContext();
-
-const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const toggleAuth = () => {
-    setIsAuthenticated((prevAuth) => !prevAuth);
-  };
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, toggleAuth }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export { AuthContext, AuthProvider };
-*/
